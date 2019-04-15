6cc381d620ac945b922e71b4862459938d787fe0
sp3006 <sushant.paricharak@gmail.com>
2019-04-15 04:24:05 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -1,45 +1,37 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time varchar(10), user_id int, level int, song_id int, artist_id int, session_id int, location varchar(10), user_agent varchar(50))
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id varchar(20), start_time varchar(10), user_id varchar(50), level varchar(50), song_id varchar(50), artist_id varchar(50), session_id varchar(50), location varchar(50), user_agent varchar(50))
 """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar(30), last_name varchar(30), gender varchar(20), level varchar(30))
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (userId varchar(50), firstName varchar(50), lastName varchar(50), gender varchar(50), level varchar(50))
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id int, title varchar(60), artist_id int, year int, duration varchar(10))
-""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id varchar(50), title varchar(50), artist_id varchar(50), year varchar(50), duration varchar(50))""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id int, name varchar(30), location varchar(30), lattitude float, longitude float)
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar(50), name varchar(50), location varchar(30), lattitude float, longitude float)""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time int, hour int, day int, week int, month int, year int, weekday int)
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time varchar(50), hour varchar(50), day varchar(50), week varchar(50), month varchar(50), year varchar(50), weekday varchar(50))
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songsplay VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)
-""")
+songplay_table_insert = ("""INSERT INTO songsplay VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)""")
 
-user_table_insert = ("""INSERT INTO users VALUES(%s, %s, %s, %s, %s)
-""")
+user_table_insert = ("""INSERT INTO users VALUES(%s, %s, %s, %s, %s)""")
 
-song_table_insert = ("""INSERT INTO songs VALUES(%s, %s, %s, %s, %s)
-""")
+song_table_insert = ("""INSERT INTO songs VALUES(%s, %s, %s, %s, %s)""")
 
-artist_table_insert = ("""INSERT INTO artists VALUES(%s, %s, %s, %s, %s)
-""")
+artist_table_insert = ("""INSERT INTO artists VALUES(%s, %s, %s, %s, %s)""")
 
-
-time_table_insert = ("""INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)
-""")
+time_table_insert = ("""INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)""")
 
 # FIND SONGS
 
